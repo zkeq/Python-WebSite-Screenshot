69797b3994d6b4ef6d81a9c4ceb69ad2772cc6b8
Zkeq <learnonly@learnonly.xyz>
2022-05-03 17:48:29 +0800

    pyppeteer 重构

--- baijiahao.py
+++ baijiahao.py
@@ -3,7 +3,7 @@ import requests
 
 # 上传图片
 def updateImage(content):
-    _upload_url = "https://baijiahao.baidu.com/builderinner/api/content/file/upload"
+    _upload_url = ""
     files = {'media': content}
     upload_res = requests.post(_upload_url, files=files)
     print('正在上传')
