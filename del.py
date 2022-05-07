dc4d4b48664a19863436a5f3fe6b41f91bd071e6
zkeq <admin@icodeq.com>
2022-05-07 18:18:19 +0800

    Update del.py

--- del.py
+++ del.py
@@ -2,17 +2,21 @@
 import json
 from urllib.parse import urlparse
 import os
+import time
 
 def get_json():
     with open('list.json', 'r') as f:
         data = json.load(f)
     return data
 
+
 data = get_json()
 for i in data:
     work_dir = urlparse(i['url']).netloc
     print("del ing:", work_dir)
     day_del = i["daydel"]
+    # 打印现在的时间戳
+    time_now = time.time()
     # 找出文件名在8天前的文件
     for root, dirs, files in os.walk(os.path.join("save",work_dir)):
         for file in files:
@@ -21,6 +25,6 @@ for i in data:
             file_time = os.path.getmtime(file_path)
             print("file time:", file_time)
             # 86400
-            if file_time < 10 * day_del:
+            if file_time < time_now - 10 * day_del:
                 os.remove(file_path)    # 删除文件
                 print("del:", file_path)
