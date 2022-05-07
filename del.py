f01e08da3732fa01082fd451a8a23492613e0456
zkeq <admin@icodeq.com>
2022-05-07 17:28:35 +0800

    123

--- del.py
+++ del.py
@@ -12,6 +12,13 @@ data = get_json()
 for i in data:
     work_dir = urlparse(i['url']).netloc
     print("del ing:", work_dir)
-    daydel = i["daydel"]
-    os.system("sh del.sh " + work_dir + " " + str(daydel))
-    print("del done:", work_dir)
+    day_del = i["day_del"]
+    # 找出文件名在8天前的文件
+    for root, dirs, files in os.walk(work_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_time = os.path.getmtime(file_path)
+            # 86400
+            if file_time < 10 * day_del:
+                os.remove(file_path)    # 删除文件
+                print("del:", file_path)
